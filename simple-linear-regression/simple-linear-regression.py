d4b8b9cf76fe22e12437531fe2a1da04beb87452
Ian Seng <ianseng@outlook.com>
2020-02-06 21:56:16 -0500

    predict salary based on the model

--- simple-linear-regression/simple-linear-regression.py
+++ simple-linear-regression/simple-linear-regression.py
@@ -19,3 +19,5 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, rando
 
 regressor = LinearRegression()
 regressor.fit(X_train, y_train);
+
+y_pred = regressor.predict(X_test)
